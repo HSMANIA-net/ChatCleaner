4b64fc779eb25d7a08bcb62f5dbfb7df4141921a
niefortunny <1niefortunny@gmail.com>
2024-02-26 00:20:24 +0100

    First release
    Initial commit

---
+++ ChatCleaner.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+
+namespace ChatCleaner
+{
+    public class ChatCleaner : BasePlugin
+    {
+        public override string ModuleName => "ChatCleaner";
+        public override string ModuleVersion => "1.0.0";
+        public override string ModuleAuthor => "unfortunate";
+
+        // TODO: Hide disconnect message
+        [GameEventHandler(HookMode.Pre)]
+        public HookResult OnPlayerTeam(EventPlayerTeam @event, GameEventInfo info)
+        {
+           @event.Silent = true;
+           info.DontBroadcast = true;
+           return HookResult.Continue;
+        }
+
+    }
+}
