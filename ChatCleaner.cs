287a80b790f2e165ef8c2412ab06afecbcbdab61
Aw3K <dedkam1@wp.pl>
2024-02-26 19:51:07 +0100

    Added EventHandler - Hide disconnect message

--- ChatCleaner.cs
+++ ChatCleaner.cs
@@ -11,7 +11,14 @@ namespace ChatCleaner
         public override string ModuleVersion => "1.0.0";
         public override string ModuleAuthor => "unfortunate";
 
-        // TODO: Hide disconnect message
+		// DONE: Hide disconnect message | by NyggaBytes
+        [GameEventHandler(HookMode.Pre)]
+        public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+        {
+            info.DontBroadcast = true;
+            return HookResult.Continue;
+        }
+
         [GameEventHandler(HookMode.Pre)]
         public HookResult OnPlayerTeam(EventPlayerTeam @event, GameEventInfo info)
         {
